c374b64a957565f5fbb250e79a04029535e4b866
Ronnylml <a1410129@iberotec.edu.pe>
2015-06-13 11:08:33 -0500

    Update TestGithub.java

--- TestGithub/src/testgithub/TestGithub.java
+++ TestGithub/src/testgithub/TestGithub.java
@@ -15,7 +15,9 @@ public class TestGithub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int a=8;
+        int b=8;
+        int x=a+b;
     }
     
 }
